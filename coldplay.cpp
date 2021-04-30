7d22c0056c73566c0570a1009639037102ca174e
Jahanvi09 <53164740+Jahanvi09@users.noreply.github.com>
2021-04-30 22:24:40 +0530

    April Lunchtime

---
+++ coldplay.cpp
@@ -0,0 +1,17 @@
+/*https://www.codechef.com/LTIME95C/problems/SLOOP*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	     int jrn; int song;
+	     cin>>jrn>>song;
+	     int c=jrn/song;
+	     cout<<c<<endl;
+	}
+	return 0;
+}
